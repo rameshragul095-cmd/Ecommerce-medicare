b23272a2550114475846e951fa23a420652770e4
rameshragul095-cmd <rameshragul095@gmail.com>
2026-05-15 13:54:18 +0530

    connected render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://medicare-backend-r6j.onrender.com/auth"
+  baseURL: "https://medicare-backend-rj0j.onrender.com"
 });
 
 export default API;
