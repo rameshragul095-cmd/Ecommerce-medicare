8a0e989d945acd1057851435f4aa75a47b36734c
rameshragul095-cmd <rameshragul095@gmail.com>
2026-05-15 14:24:43 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://ecommerce-medicare-p4c3.vercel.app",
     methods: ["GET", "POST"],
     credentials: true
   })
@@ -14,17 +14,16 @@ app.use(
 app.use(express.json());
 
 // ROUTES
-
 app.use("/auth", require("./routes/authRoutes"));
 
 // TEST
-
 app.get("/", (req, res) => {
   res.send("Backend Working");
 });
 
 // SERVER
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
