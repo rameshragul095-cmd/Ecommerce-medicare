35c0f6f260ea2fecaa2341ce5f58a910a8d33bd0
rameshragul095-cmd <rameshragul095@gmail.com>
2026-05-15 14:08:49 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 // ROUTES
